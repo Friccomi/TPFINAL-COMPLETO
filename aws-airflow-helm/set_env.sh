197893b2342c691fcfcaaed2fa25e19f65cd31cd
Florencia Riccomi <85568530+Friccomi@users.noreply.github.com>
2022-05-01 23:14:29 -0300

    Add files via upload

---
+++ aws-airflow-helm/set_env.sh
@@ -0,0 +1,10 @@
+export AIRFLOW_NAME="Airflow-on-Kubernetes"
+export AIRFLOW_NAMESPACE="airflow"
+export RELEASE_NAME="airflow-on-kubernetes"
+export CHART_VERSION=8.5.3
+export VALUES_FILE=./values.yaml
+
+#export RELEASE_NAME=airflow-kubernetes
+#export NAMESPACE=airflow
+#export CHART_VERSION=8.5.3
+#export VALUES_FILE=./values.yaml
